e712a2bc452a8e3f7579a56a55c9782963f45111
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2024-05-22 20:40:50 -0600

    Update Almacen.cpp

--- Almacen.cpp
+++ Almacen.cpp
@@ -1,25 +1,31 @@
 #include "Almacen.h"
 
+// Método getter para obtener la capacidad del almacén
 int Almacen::getCapacidad() const {
     return capacidad;
 }
 
+// Método setter para establecer la capacidad del almacén
 void Almacen::setCapacidad(int capacidad) {
     this->capacidad = capacidad;
 }
 
+// Método getter para obtener la fecha y hora de llegada al almacén
 std::time_t Almacen::getLlegada() const {
     return llegada;
 }
 
+// Método setter para establecer la fecha y hora de llegada al almacén
 void Almacen::setLlegada(std::time_t llegada) {
     this->llegada = llegada;
 }
 
+// Método getter para obtener la descripción del almacén
 std::string Almacen::getDescripcion() const {
     return descripcion;
 }
 
+// Método setter para establecer la descripción del almacén
 void Almacen::setDescripcion(const std::string& descripcion) {
     this->descripcion = descripcion;
 }
