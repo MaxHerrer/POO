7c41076db4c13227f5968c60d4729963111a2b58
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2024-06-09 21:47:45 -0600

    Update Envio.h

--- Envio.h
+++ Envio.h
@@ -5,17 +5,10 @@
 #include <ctime>
 #include "Producto.h"
 #include "Paqueteria.h"
-#include "Lugar.h" 
+#include "Lugar.h"
 
+// Clase abstracta Envio
 class Envio {
-public:
-    Envio(std::time_t llegada, const Producto& producto, const Paqueteria& paqueteria, const std::string& estado, const std::string& ciudad, const std::string& direccion)
-        : llegada(llegada), producto(producto), paqueteria(paqueteria), estado(estado), ciudad(ciudad), direccion(direccion) {}
-
-    virtual std::string getInfo() const = 0;
-    std::time_t getLlegada() const { return llegada; }
-    const Producto& getProducto() const { return producto; } 
-
 protected:
     std::time_t llegada;
     Producto producto;
@@ -23,6 +16,19 @@ protected:
     std::string estado;
     std::string ciudad;
     std::string direccion;
+
+public:
+    // Constructor de Envio
+    Envio(std::time_t llegada, const Producto& producto, const Paqueteria& paqueteria, const std::string& estado, const std::string& ciudad, const std::string& direccion)
+        : llegada(llegada), producto(producto), paqueteria(paqueteria), estado(estado), ciudad(ciudad), direccion(direccion) {}
+
+    // Método virtual puro
+    virtual std::string getInfo() const = 0;
+
+    // Método de acceso
+    std::time_t getLlegada() const { return llegada; }
+    const Producto& getProducto() const { return producto; }
+
 };
 
 #endif // ENVIO_H
