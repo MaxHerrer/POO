4f6ac4314ad337f68aae25e96312386770ed10cc
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2024-06-09 21:50:45 -0600

    Update EnvioConcreto.h

--- EnvioConcreto.h
+++ EnvioConcreto.h
@@ -1,16 +1,20 @@
 #ifndef ENVIOCONCRETO_H
 #define ENVIOCONCRETO_H
 
-#include "Envio.h" 
+#include "Envio.h"
 
+//Clase EnvioConcreto
 class EnvioConcreto : public Envio {
 public:
+    // Constructor de EnvioConcreto
     EnvioConcreto(std::time_t llegada, const Producto& producto, const Paqueteria& paqueteria, const std::string& estado, const std::string& ciudad, const std::string& direccion)
         : Envio(llegada, producto, paqueteria, estado, ciudad, direccion) {}
 
+    // Sobreescritura del método getInfo
     virtual std::string getInfo() const override {
         return "Información específica de EnvioConcreto";
     }
 };
 
-#endif // ENVIO_CONCRETO_H
+#endif // ENVIOCONCRETO_H
+
